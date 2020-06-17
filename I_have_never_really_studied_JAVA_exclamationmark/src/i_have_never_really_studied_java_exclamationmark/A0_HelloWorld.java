ef39501b28fcb914c8308d53264fe6ea31229c39
C.Hyuk JANG <59091041+chcjlhu7@users.noreply.github.com>
2020-06-18 00:24:04 +0900

    First Java Coding

---
+++ I_have_never_really_studied_JAVA_exclamationmark/src/i_have_never_really_studied_java_exclamationmark/A0_HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package i_have_never_really_studied_java_exclamationmark;
+
+/**
+ *
+ * @author C. Hyuk JANG
+ */
+public class A0_HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+       System.out.println("Hello World!");
+    }    
+}
---
+++ I_have_never_really_studied_JAVA_exclamationmark/src/i_have_never_really_studied_java_exclamationmark/I_have_never_really_studied_JAVA_exclamationmark.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package i_have_never_really_studied_java_exclamationmark;
+
+/**
+ *
+ * @author C. Hyuk JANG
+ */
+public class I_have_never_really_studied_JAVA_exclamationmark {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
